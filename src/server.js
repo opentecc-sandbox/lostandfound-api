70d7db17d90edb63290c7b9aa2ba62efc7bb1cdb
hajar <hajarchoukaih@gmail.com>
2026-03-30 19:03:45 +0100

    add

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ const allowedOrigins = [
   "http://localhost:5173", 
   process.env.FRONTEND_URL
 ];
-app.use(cors({
+/*app.use(cors({
   origin: function (origin, callback) {
     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
       callback(null, true);
@@ -20,12 +20,13 @@ app.use(cors({
   },
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
-}));
+}));*/
 
 app.use(express.json());
 
 
 app.use("/api/items", itemsRouter);
+api.use("/api/auth",authRouter);
 
 
 app.get("/", (req, res) => {
